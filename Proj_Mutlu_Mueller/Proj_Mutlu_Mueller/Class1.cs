154d0c1004c15420561daaa6fac152829f425de3
Martinampc <muellermartinjohannes@gmail.com>
2020-05-21 10:54:34 +0200

    Notenarrays und Parkplätze für Hauptfächer erstellt

---
+++ Proj_Mutlu_Mueller/Proj_Mutlu_Mueller/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Mutlu_Mueller
+{
+    class Class1
+    {
+        int[] MatheNote = new int[5];
+        int Matheparkplatz = 0;
+        int[] EnglischNote = new int[5];
+        int Englischparkplatz =0;
+        int[] DeutschNote = new int[5];
+        int Deutschparkplatz = 0;
+    }
+}
