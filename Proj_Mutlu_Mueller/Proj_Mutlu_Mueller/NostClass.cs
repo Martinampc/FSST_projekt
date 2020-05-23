63152d365c0893b1c24be5ffb893b571209fc125
Martinampc <muellermartinjohannes@gmail.com>
2020-05-23 10:54:31 +0200

    Namen der Klasse geändert. Noten und Parkplatzklasse erstellt und mit variablen ausgestattet. (als nächstes konstruktoren)

---
+++ Proj_Mutlu_Mueller/Proj_Mutlu_Mueller/NostClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Mutlu_Mueller
+{
+    class Noten
+    {
+
+        int _mathenote1jg;
+        int _mathenote2jg;
+        int _mathenote3jg;
+        int _mathenote4jg;
+        int _mathenote5jg;
+
+        int _englischnote1jg;
+        int _englischnote2jg;
+        int _englischnote3jg;
+        int _englischnote4jg;
+        int _englischnote5jg;
+
+        int _deutschnote1jg;
+        int _deutschnote2jg;
+        int _deutschnote3jg;
+        int _deutschnote4jg;
+        int _deutschnote5jg;
+
+    }
+}
+    class Parkplatz
+    {
+        int _matheparkplatz = 0;
+        int _englischparkplatz = 0;
+        int _deutschparkplatz = 0;
+    }
