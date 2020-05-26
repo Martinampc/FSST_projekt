aae97510941f6c0d187bc63177ac20aae59fa914
Martinampc <muellermartinjohannes@gmail.com>
2020-05-26 22:00:27 +0200

    i dont know what im doing anymore pls help

--- Proj_Mutlu_Mueller/Proj_Mutlu_Mueller/NostClass.cs
+++ Proj_Mutlu_Mueller/Proj_Mutlu_Mueller/NostClass.cs
@@ -26,24 +26,35 @@ namespace Proj_Mutlu_Mueller
         bool _d5er4;
         bool _d5er5;
 
-        int _jahrgang;
+        int _jahrgang; //Derzeitiger Jahrgang
+        int _wiederholungen; //Anzahl der Klassenwiderholungen
+        bool _fail; //unausbesserbarer Fehlschlag (Schulverweis)
+        
 
         //Parkplätze für unsere unterstützten Fächer
         int _matheparkplatz;
         int _englischparkplatz;
         int _deutschparkplatz;
 
-        public Noten(bool m5er1 = false, bool m5er2 = false, bool m5er3 = false, bool m5er4 = false, bool m5er5 = false, //Basis Konstruktor
+        public Noten(bool m5er1 = false, bool m5er2 = false, bool m5er3 = false, bool m5er4 = false, bool m5er5 = false, //Konstruktor
                     bool e5er1 = false, bool e5er2 = false, bool e5er3 = false, bool e5er4 = false, bool e5er5 = false,
-                    bool d5er1 = false, bool d5er2 = false, bool d5er3 = false, bool d5er4 = false, bool d5er5 = false, 
-                    int jahrgang = 0, int matheparkplatz = 0, int deutschparkplatz = 0, int englischparkplatz = 0)
+                    bool d5er1 = false, bool d5er2 = false, bool d5er3 = false, bool d5er4 = false, bool d5er5 = false,
+                    int jahrgang = 0, int wiederholungen = 0, bool fail = false,
+                    int matheparkplatz = 0, int deutschparkplatz = 0, int englischparkplatz = 0)
         {
-            if(jahrgang < 0|| jahrgang > 5)
+            if(jahrgang < 0|| jahrgang > 6)
             {
                 throw new Exception("ungültiger jahrgang");
             }
 
+            if(wiederholungen < 0 || wiederholungen > 2)
+            {
+                throw new Exception("Ungültige Anzahl der Wiederholungen");
+            }
+
             _jahrgang = jahrgang;
+            _wiederholungen = wiederholungen;
+            _fail = fail;
 
             _d5er1 = d5er1;
             _d5er2 = d5er2;
@@ -77,7 +88,7 @@ namespace Proj_Mutlu_Mueller
             _deutschparkplatz = deutschparkplatz;
             _englischparkplatz = englischparkplatz;
         }
-        //get-set Methoden für alle variablen setzten
+        //get-set Methoden für alle Noten variablen setzten
         public bool M5er1
         {
             get
@@ -254,6 +265,61 @@ namespace Proj_Mutlu_Mueller
                 _d5er5 = value;
             }
         }
+
+        public bool Fail
+        {
+            get
+            {
+                if (_deutschparkplatz == 1 && _englischparkplatz == 1 && _matheparkplatz == 1 && _jahrgang == 6) //ende der 5. Klasse müssen alle Parkplätze gereumt sein
+                {
+                    _fail = true;
+                }
+                if (_deutschparkplatz >= 2 || _matheparkplatz >=2 || _englischparkplatz >=2) //Man dar nur einen 5er pro Fach haben
+                {
+                    _fail = true;
+                }
+
+                return _fail;
+            }
+            set
+            {
+                _fail = value;
+            }
+        }
+
+        public int Wiederholungen
+        {
+            get
+            {
+                return _wiederholungen;
+            }
+            set
+            {
+                if (value < 0 || value > 2)
+                {
+                    throw new Exception("Ungültige Anzahl der Wiederholungen");
+                }
+
+                _wiederholungen = value;
+            }
+        }
+
+        public int Jahrgang
+        {
+            get
+            {
+                return _jahrgang;
+            }
+            set
+            {
+                if (value < 0 || value > 6)
+                {
+                    throw new Exception("ungültiger jahrgang");
+                }
+
+                _jahrgang = value;
+            }
+        }
         //Get Funktionen für Parkplätze
         public int Matheparkplatz
         {
@@ -279,6 +345,8 @@ namespace Proj_Mutlu_Mueller
                 return _englischparkplatz;
             }
         }
+
+       
 
         //funktionen zum managen der Parkplätze
         public void EnglischparkplatzAdd()
