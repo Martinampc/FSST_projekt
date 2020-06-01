0260fb6429361ed2f3104fddcf50956a04b7a586
Martinampc <muellermartinjohannes@gmail.com>
2020-06-01 17:12:41 +0200

    Mathe und Englischnoten abfrage. Fail test

--- Proj_Mutlu_Mueller/Proj_Mutlu_Mueller/MainWindow.xaml.cs
+++ Proj_Mutlu_Mueller/Proj_Mutlu_Mueller/MainWindow.xaml.cs
@@ -33,34 +33,92 @@ namespace Proj_Mutlu_Mueller
         private void BtnAufstieg_Click(object sender, RoutedEventArgs e)
         {
 
-            int jahrgang = Noten.Jahrgang; //derzeitigen Jahrgang auslesen
-            Noten.Jahrgang = jahrgang++; //Jahrgang aufsteigen
+            Noten.Aufsteigen(); //Jahrgang + 1
 
-            if(/*RadiobuttonDeutsch = Negativ */ && jahrgang == 1)
+            //Deutschnoten abfragen
+            if(/*RadiobuttonDeutsch = Negativ */ && Noten.Jahrgang == 1)
             {
                 Noten.D5er1 = true;
             }
 
-            if (/*RadiobuttonDeutsch = Negativ */ && jahrgang == 2)
+            if (/*RadiobuttonDeutsch = Negativ */ && Noten.Jahrgang == 2)
             {
                 Noten.D5er2 = true;
             }
 
-            if (/*RadiobuttonDeutsch = Negativ */ && jahrgang == 3)
+            if (/*RadiobuttonDeutsch = Negativ */ && Noten.Jahrgang == 3)
             {
                 Noten.D5er3 = true;
             }
 
-            if (/*RadiobuttonDeutsch = Negativ */ && jahrgang == 4)
+            if (/*RadiobuttonDeutsch = Negativ */ && Noten.Jahrgang == 4)
             {
                 Noten.D5er4 = true;
             }
 
-            if (/*RadiobuttonDeutsch = Negativ */ && jahrgang == 5)
+            if (/*RadiobuttonDeutsch = Negativ */ && Noten.Jahrgang == 5)
             {
                 Noten.D5er5 = true;
             }
 
+            //Mathenoten abfragen
+            if (/*RadiobuttonMathe = Negativ */ && Noten.Jahrgang == 1)
+            {
+                Noten.M5er1 = true;
+            }
+
+            if (/*RadiobuttonMathe = Negativ */ && Noten.Jahrgang == 2)
+            {
+                Noten.M5er2 = true;
+            }
+
+            if (/*RadiobuttonMathe = Negativ */ && Noten.Jahrgang == 3)
+            {
+                Noten.M5er3 = true;
+            }
+
+            if (/*RadiobuttonMathe = Negativ */ && Noten.Jahrgang == 4)
+            {
+                Noten.M5er4 = true;
+            }
+
+            if (/*RadiobuttonMathe = Negativ */ && Noten.Jahrgang == 5)
+            {
+                Noten.M5er5 = true;
+            }
+
+            //Englischnoten Abfragen
+            if (/*RadiobuttonEnglisch = Negativ */ && Noten.Jahrgang == 1)
+            {
+                Noten.E5er1 = true;
+            }
+
+            if (/*RadiobuttonEnglisch = Negativ */ && Noten.Jahrgang == 2)
+            {
+                Noten.E5er2 = true;
+            }
+
+            if (/*RadiobuttonEnglisch = Negativ */ && Noten.Jahrgang == 3)
+            {
+                Noten.E5er3 = true;
+            }
+
+            if (/*RadiobuttonEnglisch = Negativ */ && Noten.Jahrgang == 4)
+            {
+                Noten.E5er4 = true;
+            }
+
+            if (/*RadiobuttonEnglisch = Negativ */ && Noten.Jahrgang == 5)
+            {
+                Noten.E5er5 = true;
+            }
+
+
+            //Überprüfen ob Schulabbruch
+            if(Noten.Fail == true)
+            {
+                //Hier Visuelles Fail symbol einfügen
+            }
         }
     }
 }
